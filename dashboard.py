49f22ccb93e9ad9c1606aa3f6ced2f01b8cf4fc6
James Foster <james16.foster@gmail.com>
2020-09-24 22:07:15 +0200

    Less hackiness

--- dashboard.py
+++ dashboard.py
@@ -5,9 +5,9 @@ from components.surf_ribbon import SurfRibbonComponent
 from components.news_list import Newslist
 import time
 from PIL import Image,ImageDraw,ImageFont
-# from waveshare import epd_driver
+from os import environ
 
-dashboard_image = Image.new('1', (640, 384), 1)  # 255: clear the frame
+dashboard_image = Image.new('1', (640, 384), 1)
 
 weather_ribbon = WeatherRibbonComponent(320,175)
 weather_ribbon_image = weather_ribbon.draw()
@@ -25,13 +25,13 @@ stock_ribbon = StockTickerRibbonComponent(640,50)
 stock_ribbon_image = stock_ribbon.draw()
 dashboard_image.paste(stock_ribbon_image,(0, weather_ribbon.height + news_list.height + 1))
 
-
-dashboard_image.save("images/merged_image.jpg","JPEG")
-dashboard_image.show()
-
-# epd = epd_driver.EPD()
-# epd.init()
-# epd.Clear()
-
-# Himage = Image.new('1', (epd.width, epd.height), 256)  # 255: clear the frame
-# epd.display(epd.getbuffer(dashboard_image))
+on_device = environ.get('ON_DEVICE', False)
+if on_device:
+    from waveshare import epd_driver
+    epd = epd_driver.EPD()
+    epd.init()
+    epd.Clear()
+    epd.display(epd.getbuffer(dashboard_image))
+else:
+    dashboard_image.save("images/merged_image.jpg","JPEG")
+    dashboard_image.show()
